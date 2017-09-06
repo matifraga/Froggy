db3b8f7aa0ca4f9911f35c8d4abc9e3d5016328b
Mati <mfraga@itba.edu.ar>
2017-09-05 22:05:40 -0300

    Enemys and collisions

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    private Rigidbody rb;
+    float speed;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        speed = 100f + Random.value * 50;
+    }
+
+    void FixedUpdate() {
+        transform.position += Time.deltaTime * speed * -transform.right;
+
+        if (transform.position.x < -57) {
+            Vector3 init = new Vector3(157 + Random.value * 70, 0,0);
+            transform.position += init;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float speed;
+    private int count;
+    public Text countText;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        speed = 40f;
+        count = 0;
+        updateScore();
+    }
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Time.deltaTime * speed * -transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Time.deltaTime * speed * transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            transform.position += Time.deltaTime * speed * -transform.up;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += Time.deltaTime * speed * transform.up;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) { //TODO: search clamp usage
+        if (other.tag.Equals("Enemy")) {
+            countText.text = "LOSER";
+            transform.position = new Vector3(0, 0, 0);
+        }
+    }
+
+    void updateScore() {
+        countText.text = "Points: " + count;
+    }
+}
