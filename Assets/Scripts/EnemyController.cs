8acf9ff11e93d6d860c8cdb999b7c90d1d56b87d
lsoncini <lsoncini@itba.edu.ar>
2017-09-06 21:51:27 -0300

    several changes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
     private Rigidbody rb;
-    float speed;
+    public float speed;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
-        speed = 100f + Random.value * 50;
     }
 
     void FixedUpdate() {
         transform.position += Time.deltaTime * speed * -transform.right;
-
         if (transform.position.x < -57) {
-            Vector3 init = new Vector3(157 + Random.value * 70, 0,0);
+            Vector3 init = new Vector3(157, 0,0);
             transform.position += init;
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,59 +1,79 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     private float speed;
     private int count;
-    public int lifes;
+    public int lives;
+    private int timer;
     public Text countText;
+    public GameObject live1;
+    public GameObject live2;
+    public GameObject live3;
+
 
     void Start() {
         rb = GetComponent<Rigidbody>();
         speed = 40f;
         count = 0;
-        lifes = 3;
+        timer = 0;
+        lives = 3;
         SetInitialPosition();
-        updateScore();
-    }
-
-    private void Update() {
-        if (transform.position.y < 20) {
-            count++;
-
-        }
-        updateScore();
     }
 
     void FixedUpdate() {
-        if (lifes > 0) {
+        if (lives > 0)
+        {
 
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
                 transform.position = new Vector3(Mathf.Clamp(transform.position.x - Time.deltaTime * speed, -41f, 81f), transform.position.y, transform.position.z);
             }
 
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
                 transform.position = new Vector3(Mathf.Clamp(transform.position.x + Time.deltaTime * speed, -41f, 81f), transform.position.y, transform.position.z);
             }
 
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
                 transform.position += Time.deltaTime * speed * -transform.up;
             }
 
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-                transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + Time.deltaTime * speed, -50f, 33f), transform.position.z);
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + Time.deltaTime * speed, -50f, 37f), transform.position.z);
+            }
+        } else
+        {
+            timer++;
+            if (timer > 180)
+            {
+                SceneManager.LoadScene(0);
             }
         }
     }
 
     void OnTriggerEnter2D(Collider2D other) { 
         switch(other.tag) {
-            case "Enemy": 
-                lifes--;
+            case "Enemy":
+                if (lives == 3)
+                    live1.SetActive(false);
+                else if (lives == 2)
+                    live2.SetActive(false);
+                else if (lives == 1)
+                {
+                    countText.text = "You lose, S: " + count;
+                    live3.SetActive(false);
+                }
+                lives--;
                 break;
             case "Win":
-                countText.text = "WINNER";
+                count++;
+                countText.text = count.ToString();
                 break;
             default:
                 break;
@@ -62,17 +82,7 @@ public class PlayerController : MonoBehaviour {
     }
 
     void SetInitialPosition() {
-        Vector3 position = new Vector3(20f,30f,0);
+        Vector3 position = new Vector3(20f,35f,0);
         transform.position = position;
-    }
-
-    void updateScore() {
-        if (lifes > 0) {
-            countText.text = "Points: " + count + "\nLifes: " + lifes;
-        } else {
-            countText.text = "HIGHSCORE: " + count;
-            countText.color = Color.red;
-            countText.fontSize = 32;
-        }
     }
 }
